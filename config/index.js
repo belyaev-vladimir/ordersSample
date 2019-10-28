f78eaa353b3375cb53675d80c4856eba10fec11c
vladimir belyaev <belyaev-vladimir@mail.ru>
2019-10-28 17:05:49 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,13 +5,26 @@ const path = require('path');
 class Config {
 
     constructor() {
-        this.nconf = require('nconf');
+        //переменные окружения, которые перекрывают конфиг по умолчанию:
+        nconf.env();
 
-        //переменные окружения, которые перекрывают конфиг по умолчанию (для удобства использования в CI\CD):
-        this.nconf.env();
+        if (nconf.get('OVERRIDES_CONFIG')) {
+            let conf = nconf.get('OVERRIDES_CONFIG');
+
+            conf = typeof conf === 'string' ? JSON.parse(conf) : conf;
+
+            if (conf) {
+                nconf.overrides(conf);
+            }
+        }
+
+        //кастомный локальный файл конфига, который перекроет конфиг по умолчанию, но не переменные окружения
+        if (nconf.get('OVERRIDES_CONFIG_FILE')) {
+            nconf.file('overrides', path.join(__dirname, nconf.get('OVERRIDES_CONFIG_FILE')));
+        }
 
         // конфиг по умолчанию
-        this.nconf.file('defaults', path.join(__dirname, 'config.json'));
+        nconf.file('defaults', path.join(__dirname, 'config.json'));
     }
 
     get(key) {
